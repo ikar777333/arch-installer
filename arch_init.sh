33728376c4b3954c085a94a238b9de30501f283e
ikar <ikar777333@gmail.com>
2019-03-30 23:12:27 +0300

    fix(init): Fix mirrorlist path and divide the script into two parts.
    Fix mirrorlist path: fixed misprint in mirrorlist path. 
    Divide the script into two parts: When we login in a new arch, scrips stop until after the chroot closes in the host environment. Now we start the second script inside the chroot environment. For more information about problem: https://bbs.archlinux.org/viewtopic.php?id=204252

--- arch_init.sh
+++ arch_init.sh
@@ -6,8 +6,8 @@
 # mkfs -t ext2 -L Lable /dev/sda*
 
 #Set link for download
-link = $(grep ".ru" /etc/pacmad.d/mirrorlist)
-sed -i '1i $link\n' /etc/pacmad.d/mirrorlist
+link = $(grep ".ru" /etc/pacman.d/mirrorlist)
+sed -i '1i $link\n' /etc/pacman.d/mirrorlist
 
 #install arch
 pacstrap /mnt base-devel base
@@ -17,41 +17,3 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 #login in
 arch-chroot /mnt
-
-#Set timezone
-ln -sf /usr/share/Europe/Moscow /etc/localtime
-hwclock --systohc --localtime
-
-#set locale
-sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen && sed -i 's/^# *\(ru_RU.UTF-8\)/\1/' /etc/locale.gen && locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-#set font
-sed -i '1i FONT=UniCyr_8x16\n' /etc/vconsole.conf
-
-#set name of pc
-echo "deus" > /etc/hostname
-sed -i '1i 127.0.0.1 deus.localdomain deus n/' /etc/hosts
-
-#set initcpio
-mkinitcpio -p linux
-
-#set superuser
-passwd
-
-#set user
-useradd -m -g users -G wheel -s /bin/bash ikar
-passwd 
-sed -i 's/^# *\(%wheel ALL=(ALL) ALL\)/\1/' /etc/sudoers
-
-#set load
-pacman -Sy os-prober grub efibootmgr
-mkdir /boot/efi
-mkdir /boot/grub
-os-prober
-grub-mkconfig -o /boot/grub/grub.cfg
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-
-
-#finish
-exit && umount -R /mnt && reboot
---
+++ arch_init_part_2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Set timezone
+ln -sf /usr/share/Europe/Moscow /etc/localtime
+hwclock --systohc --localtime
+
+#set locale
+echo "en_US.UTF-8" > /etc/locale.gen
+echo "ru_RU.UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+#set font
+sed -i '1i FONT=UniCyr_8x16\n' /etc/vconsole.conf
+
+#set name of pc
+echo "deus" > /etc/hostname
+sed -i '1i 127.0.0.1 deus.localdomain deus n/' /etc/hosts
+
+#set initcpio
+mkinitcpio -p linux
+
+#set superuser
+passwd
+
+#set user
+useradd -m -g users -G wheel -s /bin/bash ikar
+passwd 
+sed -i 's/^# *\(%wheel ALL=(ALL) ALL\)/\1/' /etc/sudoers
+
+#set load
+pacman -Sy os-prober grub efibootmgr
+mkdir /boot/efi
+mkdir /boot/grub
+os-prober
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+
+
+#finish
+exit && umount -R /mnt && reboot
