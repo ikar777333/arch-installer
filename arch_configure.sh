fc0cbb05015ccb624cbc7831fdbacffd3f8a6d5c
ikar <ikar777333@gmail.com>
2019-04-10 17:57:11 +0300

    test

--- arch_configure.sh
+++ arch_configure.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm xf86-video-vesa &
-sudo pacman -S --noconfirm xorg-server xorg-apps xorg-xinit mesa &
+sudo pacman -S --noconfirm xf86-video-vesa 
+sudo pacman -S --noconfirm xorg-server xorg-apps xorg-xinit mesa 
 sudo pacman -S --noconfirm xterm
 
 cd /home/ikar/
 Xorg :0 -configure
 sudo cp /root/xorg.conf.new /etc/X11/xorg.conf
-sudo cp /arch_installer_scripts/.xinitrc /home/ikar/
+sudo cp /arch_installer_scripts/configs/.xinitrc /home/ikar/
 
-sudo pacman -S --noconfirm alsa-utils alsa-plugins &
-sudo pacman -S --noconfirm ranger wget chromium &
+sudo pacman -S --noconfirm alsa-utils alsa-plugins 
+sudo pacman -S --noconfirm ranger wget chromium 
 
 #Visual install
 sudo pacman -S --noconfirm i3 slim rofi feh
@@ -26,6 +26,7 @@ makepkg -si
 sudo pacman -S --noconfirm zsh
 sudo chsh -s /bin/zsh ikar
 sudo chsh -s /bin/zsh root
+zsh
 zsh /arch_installer_scripts/install.sh
 #wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
