84137f871d0eda2ffa159e4de57eaf2f51d3049b
ikar <ikar777333@gmail.com>
2019-04-10 05:49:31 +0300

    test

--- arch_configure.sh
+++ arch_configure.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-sudo su -
 
-pacman -S --noconfirm xf86-video-vesa
-pacman -S --noconfirm xorg-server xorg-apps xorg-xinit mesa
-pacman -S --noconfirm xterm
+sudo pacman -S --noconfirm xf86-video-vesa &
+sudo pacman -S --noconfirm xorg-server xorg-apps xorg-xinit mesa &
+sudo pacman -S --noconfirm xterm
 
-sudo -u ikar Xorg :0 -configure
-cp /root/xorg.conf.new /etc/X11/xorg.conf
-sudo -u ikar wget -P /home/ikar/ https://github.com/ikar777333/arch-installer/raw/prototype_of_configure/configs/.xinitrc
+cd /home/ikar/
+Xorg :0 -configure
+sudo cp /root/xorg.conf.new /etc/X11/xorg.conf
+wget https://github.com/ikar777333/arch-installer/raw/prototype_of_configure/configs/.xinitrc
 
-pacman -S --noconfirm alsa-utils alsa-plugins
-pacman -S --noconfirm ranger wget chromium 
+sudo pacman -S --noconfirm alsa-utils alsa-plugins &
+sudo pacman -S --noconfirm ranger wget chromium &
 
 #Visual install
-pacman -S --noconfirm i3 slim rofi feh
+sudo pacman -S --noconfirm i3 slim rofi feh
 #systemctl enable slim.service
 
 #Yey install
 cd /home/ikar/
-sudo -u ikar git clone https://aur.archlinux.org/yay.git
-sudo -u ikar cd yay
-sudo -u ikar makepkg -si
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
 #Zsh install
 pacman -S --noconfirm zsh
-sudo -u ikar zsh
-chsh -s /bin/zsh root
-chsh -s /bin/zsh ikar
-sudo -u ikar wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+ikar zsh
+sudo chsh -s /bin/zsh ikar
+sudo chsh -s /bin/zsh root
+#wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
 #copy configs files
